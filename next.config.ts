86fe1e28d51e5eb722be8f161f255da6af3f2457
watanabe-tsubasa <t.watanabe423@gmail.com>
2025-12-01 00:01:11 +0900

    ver.0.1

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,22 @@ import type { NextConfig } from "next";
 import { withWorkflow } from "workflow/next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{ protocol: "https", hostname: "lh3.googleusercontent.com" },
-			{ protocol: "https", hostname: "ik.imagekit.io" },
-			{ protocol: "https", hostname: "placehold.co" },
-		],
-	},
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "ik.imagekit.io" },
+      { protocol: "https", hostname: "placehold.co" },
+    ],
+  },
+
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+
+  webpack(config) {
+    config.externals.push("@prisma/client", "prisma");
+    return config;
+  },
 };
 
 export default withWorkflow(nextConfig);
